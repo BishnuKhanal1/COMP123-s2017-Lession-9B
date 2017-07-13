382610b0705f626177ff036a7649573ea2d65862
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-12 21:24:13 -0400

    Added SuperHuman class

---
+++ COMP123-s2017-Lession 9B/SuperHuman.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name: Bishnu Khanal
+ * Date: July 11, 2017
+ * Description: This is the SuperHuman class that extends the Human abstract class
+ * Version:0.1 - Created the SuperHuman class
+ */
+namespace COMP123_s2017_Lession_9B
+{
+    /// <summary>
+    /// This is the SuperHuman class
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        //Private instant variables
+
+        //Public properties
+
+        //Constructors
+
+        public SuperHuman(string name)
+            :base(name)
+        {
+            
+        }
+        //Private Methods
+
+        //Public Methods
+    }
+}

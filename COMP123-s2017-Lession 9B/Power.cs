4415403202df7430ee64ad68ab9ca2ce4922df60
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-12 21:30:24 -0400

    Added Power class

---
+++ COMP123-s2017-Lession 9B/Power.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name: Bishnu Khanal
+ * Date: July 11, 2017
+ * Description: This is the Power class
+ * Version:0.1 - Created the Power class
+ */
+namespace COMP123_s2017_Lession_9B
+{
+    /// <summary>
+    /// This is the Power class which will be used as a Date type
+    /// </summary>
+    public class Power
+    {
+        //Publci properties
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}

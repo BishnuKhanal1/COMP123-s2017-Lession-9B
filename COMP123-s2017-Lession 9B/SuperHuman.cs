b992946b2427105c916d8800d98b3b9144903902
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-12 21:43:01 -0400

    Added _initialized method to SuperHuman class

--- COMP123-s2017-Lession 9B/SuperHuman.cs
+++ COMP123-s2017-Lession 9B/SuperHuman.cs
@@ -5,7 +5,7 @@ using System.Text;
 /* Name: Bishnu Khanal
  * Date: July 11, 2017
  * Description: This is the SuperHuman class that extends the Human abstract class
- * Version:0.1 - Created the SuperHuman class
+ * Version:0.2 - Added _initialized method
  */
 namespace COMP123_s2017_Lession_9B
 {
@@ -15,18 +15,28 @@ namespace COMP123_s2017_Lession_9B
     public class SuperHuman : Human
     {
         //Private instant variables
-
+        private List<Power> _powers;
         //Public properties
 
-        //Constructors
-
+        //Constructor
+        /// <summary>
+        /// This is the main constructor for the SuperHuman class.
+        /// It takes one parameter - name(string)
+        /// </summary>
+        /// <param name="name"></param>
         public SuperHuman(string name)
             :base(name)
         {
-            
+            this._initialized();
         }
         //Private Methods
-
+        /// <summary>
+        /// This method initializes, instantiates and assigns values to class properties
+        /// </summary>
+        private void _initialized()
+        {
+            this._powers = new List<Power>();//creates an empty list
+        }
         //Public Methods
     }
 }

a0476592ba22086fc697725768df3b8acf0dbc7b
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-12 20:46:31 -0400

    Modified program.cs

--- COMP123-s2017-Lession 9B/Program.cs
+++ COMP123-s2017-Lession 9B/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* Name: Bishnu Khanal
+ * Date: July 11, 2017
+ * Description: Demo for Lesson 9
+ * Version:0.1 - Created the Program class
+ */
 namespace COMP123_s2017_Lession_9B
 {
     class Program
